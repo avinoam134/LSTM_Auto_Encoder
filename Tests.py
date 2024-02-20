e59bfc08967761e99dc0e784a8bfd45205881975
Avinoam David <avinoam134@gmail.com>
2024-02-20 21:07:40 +0200

    P3.1 works fine

--- Tests.py
+++ Tests.py
@@ -1,54 +1,67 @@
-import lstm_ae_toy as p1
+from lstm_ae_toy import LSTM_AE_TOY, generate_syntethic_data, load_syntethic_data
 import matplotlib.pyplot as plt
 import subprocess
+import numpy as np
+import torch
+import os
 
 
 def P1_Q1_plot_signal_vs_time():
-    dataset = p1.generate_syntethic_data()
+    dataset = generate_syntethic_data()
     plt.plot(dataset[0])
     plt.show()
 
 
-def find_best_perfoming_hyperparameters():
+def find_best_perfoming_model(dry_run=False):
+    if dry_run and os.path.exists('lstm_ae_toy_model.pth'):
+        return torch.load('lstm_ae_toy_model.pth').eval()
     learning_rates = [0.001, 0.01, 0.1]
-    hidden_state_sizes = [16, 32, 64, 128]
-    gradient_clipping = [1, 5, 10]
-    epochs = 1000
+    hidden_state_sizes = [8, 16, 32]
+    gradient_clipping = [1, 5]
+    epochs = 100
+    best_params = [0,0,0,0]
     best_loss = float('inf')
-    best_hyperparameters = None
-    for learning_rate in learning_rates:
-        for hidden_state_size in hidden_state_sizes:
-            for gradient_clip in gradient_clipping:
-                #run lst_ae_toy.py script with the hyperparameters as args using os.subprocess:
+    best_model = None
+    for i,learning_rate in enumerate(learning_rates):
+        for j,hidden_state_size in enumerate(hidden_state_sizes):
+            for k,gradient_clip in enumerate(gradient_clipping):
                 result = subprocess.run(['python3', 'lstm_ae_toy.py',
-                                        '--optimiser', 'Adam',
+                                         '--input_size', '1',
+                                        '--optimizer', 'Adam',
                                         '--learning_rate', str(learning_rate),
-                                        '--hidden_state_size', str(hidden_state_size),
+                                        '--hidden_size', str(hidden_state_size),
                                         '--epochs', str(epochs),
                                         '--gradient_clipping', str(gradient_clip)],
                                         text=True, capture_output=True)
-                print (result.stderr)
                 curr_loss = float(result.stdout)
-                
+                print (f'iteration {i}.{j}.{k}:\n learning_rate: {learning_rate}, hidden_state_size: {hidden_state_size}, gradient_clip: {gradient_clip}, final_loss: {curr_loss}')
                 if curr_loss < best_loss:
                     best_loss = curr_loss
-                    best_hyperparameters = (learning_rate, hidden_state_size, gradient_clip)
-    return best_hyperparameters
+                    best_params = (learning_rate, hidden_state_size, gradient_clip)
+                    best_model = torch.load('lstm_ae_toy_model.pth').eval()
+
+    print (f'grid search done. best validation loss: {best_loss}. learning_rate: {best_params[0]}, hidden_state_size: {best_params[1]}, gradient_clip: {best_params[2]}')
+    torch.save(best_model, 'lstm_ae_toy_model.pth')
+    return best_model
 
 def Q2_select_hyperparameters_and_train_model():
-    hyperparams = find_best_perfoming_hyperparameters()
-    train_loader, test_loader, val_loader = p1.load_syntethic_data(128)
-    model = p1.LSTM_AE(10, hyperparams[1])
-    p1.train(model, p1.nn.MSELoss(), p1.get_optimizer('Adam', model, hyperparams[0]),
-            train_loader, 1000, hyperparams[2])
+    model = find_best_perfoming_model(dry_run=True)
+    _, test_loader, _ = load_syntethic_data(128)
     #make test_samples contain 2 single samples from the test_loader:
-    test_samples = [next(iter(test_loader))[0], next(iter(test_loader))[0]]
+    test_samples = [next(iter(test_loader))[0].unsqueeze(-1), next(iter(test_loader))[0].unsqueeze(-1)]
+    #make test samples as a tensor:
+    test_samples = torch.tensor(np.array(test_samples))
     #make test_samples_reconstruction contain the reconstruction of the samples in test_samples:
     test_samples_reconstruction = model(test_samples)
-    #plot the original and reconstructed samples:
-    plt.plot(test_samples[0].detach().numpy())
-    plt.plot(test_samples_reconstruction[0].detach().numpy())
+    #plot the original and reconstructed samples on the same plot (side by side):
+    fig, ax = plt.subplots(1, 2)
+    for i in range(2):
+        ax[i].set_title(f"Sample {i+1}")
+        ax[i].plot(test_samples[i].detach().numpy(), linewidth=2.5)
+        ax[i].plot(test_samples_reconstruction[i].detach().numpy(), linewidth=5, alpha=0.5)
+        ax[i].legend(['Original', 'Reconstruction'], loc='upper right')
     plt.show()
+
 
 def main():
     Q2_select_hyperparameters_and_train_model()
--- lstm_ae_toy.py
+++ lstm_ae_toy.py
@@ -6,28 +6,35 @@ import argparse
 import random
 import numpy as np
 
-class LSTM_AE(nn.Module):
-    def __init__(self, input_size=10, hidden_state_size=64):
-        super(LSTM_AE, self).__init__()
-        self.encoder = nn.LSTM(input_size, hidden_state_size, batch_first=True)
-        self.decoder = nn.LSTM(hidden_state_size, input_size, batch_first=True)
+class LSTM_AE_TOY(nn.Module):
+    def __init__(self, input_size=1, hidden_size=64, layers=1):
+        super(LSTM_AE_TOY, self).__init__()
+        self.hidden_size = hidden_size
+        self.input_size = input_size
+        self.layers = layers
+        self.encoder = nn.LSTM(input_size, hidden_size,layers, batch_first=True)
+        self.decoder = nn.LSTM(hidden_size, input_size, layers, batch_first=True)
 
     def forward(self, x):
-        _, (hidden_state, _) = self.encoder(x)
-        output, _ = self.decoder(hidden_state)
-        return output
+        batch_size = x.size(0)
+        ench0c0 = [torch.zeros(self.layers, batch_size, self.hidden_size)]*2
+        dech0c0 = [torch.zeros(self.layers, batch_size, self.input_size)]*2
+        enc, _ = self.encoder(x, (ench0c0[0], ench0c0[1]))
+        dec, _ = self.decoder(enc, (dech0c0[0], dech0c0[1]))
+        return dec
     
-
 def train(model, criterion, optimizer, train_loader, epochs, gradient_clipping):
+    model.train()
     for epoch in range(epochs):
         for batch in train_loader:
+            batch = batch.unsqueeze(-1)
             optimizer.zero_grad()
-            output = model(batch)
-            loss = criterion(output, batch)
+            outputs = model(batch)
+            loss = criterion(outputs, batch)
             loss.backward()
             nn.utils.clip_grad_norm_(model.parameters(), gradient_clipping)
             optimizer.step()
-    torch.save(model.state_dict(), 'lstm_ae_model.pth')
+    torch.save(model, 'lstm_ae_toy_model.pth')
 
 
 def test(model, test_loader, criterion):
@@ -35,12 +42,12 @@ def test(model, test_loader, criterion):
     test_loss = 0
     with torch.no_grad():
         for data in test_loader:
+            data = data.unsqueeze(-1)
             outputs = model(data)
             loss = criterion(outputs, data)
             test_loss += loss.item()
     average_test_loss = test_loss / len(test_loader)
     return average_test_loss
-
 
 
 def get_optimizer(optimizer_name, model, learning_rate):
@@ -63,7 +70,7 @@ def generate_syntethic_data(num_sequences=10000, sequence_length=50):
         i = random.randint(20, 30)
         sequence[i-5:i+6] *= 0.1
         dataset.append(sequence)
-    dataset = torch.tensor(dataset, dtype=torch.float32)
+    dataset = torch.tensor(np.array(dataset), dtype=torch.float32)
     return dataset
 
 def split_dataset(dataset, batch_size, train_size=0.6, test_size=0.2):
@@ -85,7 +92,7 @@ def load_syntethic_data(batch_size=128):
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--input_size', type=int, default=10)
+    parser.add_argument('--input_size', type=int, default=1)
     parser.add_argument('--hidden_size', type=int, default=64)
     parser.add_argument('--batch_size', type=int, default=128)
     parser.add_argument('--epochs', type=int, default=1000)
@@ -105,7 +112,7 @@ def init_train_and_validate(model, args, train_loader, val_loader):
 
 def main():
     args = parse_args()
-    model = LSTM_AE(args.input_size, args.hidden_size)
+    model = LSTM_AE_TOY(args.input_size, args.hidden_size)
     train_loader, _, val_loader = load_syntethic_data(args.batch_size)
     loss = init_train_and_validate(model, args, train_loader, val_loader)
     print(loss)
